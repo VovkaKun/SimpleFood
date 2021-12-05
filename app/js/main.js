936fa16d5c75cc0b4eff965bb92c229026e3f32a
VovkaKun <kuntiyvolodymyr@gmail.com>
2021-12-05 17:27:08 +0300

    правки по 14 ДЗ

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,15 @@
 $(function () {
 
-  $('.burger, .menu a, .address').on('click', function () {
+  $('.burger, .menu__list, .address').on('click', function () {
     $(this).toggleClass('active');
-    $('.menu__list').toggleClass('active');
-    $('.address').toggleClass('active');
+  });
+
+  $('.menu__list, .address').on('click', function () {
+    $('.burger').removeClass('active');
+    $('.body').removeClass('bg');
+    $('.popular__btn').removeClass('trans');
+    $('.product-card ').removeClass('trans');
+    $('.hero__desc ').removeClass('trans');
   });
 
   $('.burger').on('click', function () {
@@ -11,17 +17,18 @@ $(function () {
     $('.popular__btn').toggleClass('trans');
     $('.product-card ').toggleClass('trans');
     $('.hero__desc ').toggleClass('trans');
+    $('.menu__list').toggleClass('active');
   });
 
 
-  var mixer = mixitup('.product')
+  var mixer = mixitup('.popular__list')
 
   $('.reviews__inner').slick({
     dots: true,
 
-    prevArrow: '<svg class="prevArrow" width="40" height="40" viewBox="0 0 40 40" fill="none" stroke="none" xmlns="http://www.w3.org/2000/svg"><circle cx="20" cy="20" r="18.5" fill-opacity="0.01" stroke-width="3"/><path d="M21.1968 29.0492L13.4508 21.3033C12.8497 20.699 12.8497 19.7231 13.4508 19.1189L21.1968 11.3729C21.7761 10.8757 22.6328 10.8757 23.2122 11.3729C23.8614 11.9291 23.9372 12.9082 23.3811 13.5573L16.7351 20.2033L23.3811 26.8648C23.9822 27.469 23.9822 28.445 23.3811 29.0492C22.7769 29.6503 21.8009 29.6503 21.1968 29.0492Z"/></svg>',
+    prevArrow: '<button class="prevarrow__btn" type="button"><svg class="prevArrow" width="34" height="34"><use xlink:href="images/images.svg#prev-arrow"></use></svg><span class="sr-only">поиск блюда</span></button>',
 
-    nextArrow: '<svg class="nextArrow" width="40" height="40" viewBox="0 0 40 40" fill="none" stroke="none" xmlns="http://www.w3.org/2000/svg"><circle cx="20" cy="20" r="18.5" fill-opacity="0.01" stroke-width="3"/><path d="M18.6352 11.4508L26.3811 19.1967C26.9822 19.801 26.9822 20.7769 26.3811 21.3811L18.6352 29.1271C18.0558 29.6243 17.1991 29.6243 16.6197 29.1271C15.9706 28.5709 15.8947 27.5918 16.4509 26.9427L23.0968 20.2967L16.4508 13.6352C15.8497 13.031 15.8497 12.055 16.4508 11.4508C17.055 10.8497 18.031 10.8497 18.6352 11.4508Z"/></svg>',
+    nextArrow: '<button class="nextarrow__btn" type="button"><svg class="nextArrow" width="34" height="34"><use xlink:href="images/images.svg#next-arrow"></use></svg><span class="sr-only">поиск блюда</span></button>',
 
     responsive: [
       {
@@ -61,6 +68,5 @@ $(function () {
   } else {
 
   }
-
 
 });
